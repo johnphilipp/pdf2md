7e5a03ec5bf3fd7b84213109e8b77837ed4d5d38
Philipp John <35657453+johnphilipp@users.noreply.github.com>
2022-09-21 08:21:37 +0200

    Implemented web app with streamlit

--- pdf2md.py
+++ pdf2md.py
@@ -1,68 +1,64 @@
-import glob
 from mdutils.mdutils import MdUtils
 import os
-from pdf2image import convert_from_path
+import pdf2image
 import shutil
+import streamlit as st
+import tempfile
 
 
-def _mkdir(dir):
+def get_md_from_path(pdf_path, pdf_fname):
     """
-    Create or overwrite new directory
+    Return path to `*.zip` from `*.pdf` path.
+    Zip includes:
+        > `img/` dir with images of each pdf slide
+        > `*.md` file with references to images in img/ dir
+    Zip built with a temporary dir using the `tempfile`
+    library and a nested dir using the standard `os` library.
+    The temporary dir is then archived as `*.zip` using the
+    `shutil` library for final assembly. The temporary dir is
+    deleted upon function return.
     """
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
-    os.makedirs(dir)
+    with st.spinner(text="In progress..."):
+        pdf_fname = "pdf2md_" + pdf_fname.rstrip(".pdf")
+        images = pdf2image.convert_from_path(pdf_path)
+        temp_root_dir = tempfile.mkdtemp()
+        os.mkdir(temp_root_dir + "/img")
+        md = MdUtils(
+            file_name=temp_root_dir + "/" + pdf_fname,
+            title=pdf_fname
+        )
+        for i, image in enumerate(images):
+            jpg_fname = "img" + str(i) + ".jpg"
+            jpg_path = temp_root_dir + "/img/" + jpg_fname
+            image.save(jpg_path, "JPEG")
+            md.new_line(
+                md.new_inline_image(
+                    text="",
+                    path="img/" + jpg_fname
+                ))
+        md = md.create_md_file()
+        zip = shutil.make_archive(
+            base_name=pdf_fname,
+            format="zip",
+            root_dir=temp_root_dir
+        )
+        shutil.rmtree(temp_root_dir)
+        return zip
 
 
-def _get_images(fname):
+def get_md_from_bytes(pdf_file, pdf_fname):
     """
-    Convert multipage pdf to image object with separate images
+    Return path to `*.zip` from `*.pdf` byte values.
+    Function creates a temporary file and calls the
+    `get_md_from_path` function for main execution.
+    The temporary file is deleted upon function return.
     """
-    return convert_from_path(fname)
-
-
-def _create_md(fname, title):
-    """
-    Create empty markdown file
-    """
-    return MdUtils("out/" + fname, title)
-
-
-def _fill_md(md, images):
-    """
-    Fill empty markdown file with title and images
-    """
-    for i, image in enumerate(images):
-        fname = "img" + str(i) + ".jpg"
-        path = "img/" + fname
-        image.save("out/" + path, "JPEG")
-        md.new_line(md.new_inline_image(text="", path=path))
-    return md
-
-
-def _save_md(md):
-    """
-    Save markdown file
-    """
-    md.create_md_file()
-
-
-def _zip(fname):
-    return shutil.make_archive(fname, 'zip', "out")
-
-
-def main():
-    _mkdir("out")
-    _mkdir("out/img")
-
-    pdf = glob.glob("your_pdf/*.pdf")[0]
-    images = _get_images(pdf)
-
-    md = _create_md("file", "Headline")
-    md = _fill_md(md, images)
-    _save_md(md)
-    _zip("out")
-
-
-if __name__ == "__main__":
-    main()
+    fh, temp_filename = tempfile.mkstemp()
+    try:
+        with open(temp_filename, "wb") as f:
+            f.write(pdf_file)
+            f.flush()
+            return get_md_from_path(f.name, pdf_fname)
+    finally:
+        os.close(fh)
+        os.remove(temp_filename)
---
+++ run_app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import os
+import pdf2md
+
+
+st.markdown("<html><h1>pdf2md</h1> </html>", unsafe_allow_html=True)
+st.markdown("<div style='margin-top:5;'></div>", unsafe_allow_html=True)
+st.markdown("#### Easily convert multi-page pdf documents to images in markdown")
+st.markdown("<div style='margin-top:10px;'></div>", unsafe_allow_html=True)
+st.markdown("""
+            <html>
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-file-earmark-pdf" viewBox="0 0 16 16">
+            <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
+            <path d="M4.603 14.087a.81.81 0 0 1-.438-.42c-.195-.388-.13-.776.08-1.102.198-.307.526-.568.897-.787a7.68 7.68 0 0 1 1.482-.645 19.697 19.697 0 0 0 1.062-2.227 7.269 7.269 0 0 1-.43-1.295c-.086-.4-.119-.796-.046-1.136.075-.354.274-.672.65-.823.192-.077.4-.12.602-.077a.7.7 0 0 1 .477.365c.088.164.12.356.127.538.007.188-.012.396-.047.614-.084.51-.27 1.134-.52 1.794a10.954 10.954 0 0 0 .98 1.686 5.753 5.753 0 0 1 1.334.05c.364.066.734.195.96.465.12.144.193.32.2.518.007.192-.047.382-.138.563a1.04 1.04 0 0 1-.354.416.856.856 0 0 1-.51.138c-.331-.014-.654-.196-.933-.417a5.712 5.712 0 0 1-.911-.95 11.651 11.651 0 0 0-1.997.406 11.307 11.307 0 0 1-1.02 1.51c-.292.35-.609.656-.927.787a.793.793 0 0 1-.58.029zm1.379-1.901c-.166.076-.32.156-.459.238-.328.194-.541.383-.647.547-.094.145-.096.25-.04.361.01.022.02.036.026.044a.266.266 0 0 0 .035-.012c.137-.056.355-.235.635-.572a8.18 8.18 0 0 0 .45-.606zm1.64-1.33a12.71 12.71 0 0 1 1.01-.193 11.744 11.744 0 0 1-.51-.858 20.801 20.801 0 0 1-.5 1.05zm2.446.45c.15.163.296.3.435.41.24.19.407.253.498.256a.107.107 0 0 0 .07-.015.307.307 0 0 0 .094-.125.436.436 0 0 0 .059-.2.095.095 0 0 0-.026-.063c-.052-.062-.2-.152-.518-.209a3.876 3.876 0 0 0-.612-.053zM8.078 7.8a6.7 6.7 0 0 0 .2-.828c.031-.188.043-.343.038-.465a.613.613 0 0 0-.032-.198.517.517 0 0 0-.145.04c-.087.035-.158.106-.196.283-.04.192-.03.469.046.822.024.111.054.227.09.346z"/>
+            </svg>
+            &nbsp
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg> 
+            &nbsp&nbsp
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
+            <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+            <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z"/>
+            </svg>
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-filetype-md" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2H9v-1h3a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM.706 13.189v2.66H0V11.85h.806l1.14 2.596h.026l1.14-2.596h.8v3.999h-.716v-2.66h-.038l-.946 2.159h-.516l-.952-2.16H.706Zm3.919 2.66V11.85h1.459c.406 0 .741.078 1.005.234.263.157.46.383.589.68.13.297.196.655.196 1.075 0 .422-.066.784-.196 1.084-.131.301-.33.53-.595.689-.264.158-.597.237-1 .237H4.626Zm1.353-3.354h-.562v2.707h.562c.186 0 .347-.028.484-.082a.8.8 0 0 0 .334-.252 1.14 1.14 0 0 0 .196-.422c.045-.168.067-.365.067-.592a2.1 2.1 0 0 0-.117-.753.89.89 0 0 0-.354-.454c-.159-.102-.362-.152-.61-.152Z"/>
+            </svg>
+            </html>
+        """, unsafe_allow_html=True)
+st.markdown("<div style='margin-top:100px;'></div>", unsafe_allow_html=True)
+
+
+pdf = st.file_uploader("Upload a pdf file", type="pdf")
+
+if pdf is not None:
+    zip_path = pdf2md.get_md_from_bytes(pdf.getvalue(), str(pdf.name))
+    try:
+        with open(zip_path, "rb") as f:
+            download_button = st.download_button(
+                label="Download file",
+                data=f,
+                file_name="pdf2md.zip"
+            )
+        if download_button:
+            st.write("Cheers for using pdf2md 🍻")
+    finally:
+        os.remove(zip_path)
